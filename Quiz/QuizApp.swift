a87a41e4e686a4fdeeb6cf93555090d37853e1cd
Gustavo Isoo Binder <gustan.binder@gmail.com>
2023-07-31 14:51:41 -0300

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by Gustavo Binder on 31/07/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
